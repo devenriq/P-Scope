559d5c4ced711264c8fafcbd4a39dda1cbffbad1
Enrique <octepc.97@gmail.com>
2022-07-31 17:15:07 -0500

    studied the new ways to declare variables: const and let. Also, watched their scopes and functionality

---
+++ src/scope/re.js
@@ -0,0 +1,20 @@
+var firstName; //se está declarando. El valor por default es undefined, porque no es igual a nada
+firstName = "Enrique";
+console.log(firstName);
+
+var lastName = "Palomino"; //declaración y asignación
+lastName = "Ana"; // esta es una reasignación del valor de la variable lastName.
+console.log(lastName);
+
+var secondName = "David"; //declaración y asignación
+var secondName = "Gilberto"; //acá se está aplicando la reasignación y redeclaración de la variable
+console.log(secondName);
+
+// uso de let
+let fruit = "Apple"; //declaracion y asignación
+fruit = "banana"; // reasignación
+
+console.log(fruit);
+
+// uso de const
+const animal = "dog"; //declaración y asignación
