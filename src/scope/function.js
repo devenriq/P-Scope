46e65636ee6b4eccc6e5664efc57f82f549925dc
Enrique <octepc.97@gmail.com>
2022-07-31 16:07:52 -0500

    reviewd the global and the function scope in JS

--- src/scope/function.js
+++ src/scope/function.js
@@ -1,18 +1,11 @@
-const fruits = () => {
-  var fruit = "Apple";
-  console.log(fruit);
-};
+function greeting() {
+  let userName = "Ana";
+  // console.log(userName);
+  if (userName === "Ana") {
+    console.log("Hello, " + userName);
+  }
+}
 
-fruits();
-// console.log(fruit);
+greeting();
 
-const anotherFunction = () => {
-  var x = 1;
-  var x = 2;
-  // let y = 1;
-  let y = 2;
-  console.log(x);
-  console.log(y);
-};
-
-anotherFunction();
+console.log(userName);
--- src/scope/global.js
+++ src/scope/global.js
@@ -1,22 +1,9 @@
-var hello = "Hello";
-let world = "Hello World";
-const helloWorld = "Hello World!";
+var a; //declarar
+var b = "b"; //inicializar
 
-const anotherFunction = () => {
-  console.log(hello);
-  console.log(world);
-  console.log(helloWorld);
-};
+var fruit = "apple";
 
-anotherFunction();
-
-const helloWorld = () => {
-  globalVar = "I'm global";
-};
-
-helloWorld();
-console.log(globalThis);
-
-const anotherFunction = () => {
-  var localVar = (globalVar = "I'm global");
-};
+function bestFruit() {
+  console.log(fruit);
+}
+bestFruit();
