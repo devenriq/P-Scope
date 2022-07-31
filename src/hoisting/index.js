ca29e17cfa8817df6f3c0b438ed77ca6f48cada0
Enrique <octepc.97@gmail.com>
2022-07-31 18:28:37 -0500

    finished the course

---
+++ src/hoisting/index.js
@@ -0,0 +1,12 @@
+// "use strict";
+// console.log(nameOfDog);
+// var nameOfDog = "shenzi";
+// console.log(nameOfDog);
+
+nameOfDog();
+
+function nameOfDog() {
+  console.log("El mejor perro es " + nameDog);
+}
+
+var nameDog = "Bobby";
